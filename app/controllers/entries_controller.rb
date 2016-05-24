611da86923cff6cc2af5e3605d6ff3a6db984353
Rachel Prows <rachel.prows@icloud.com>
2016-05-24 12:04:07 -0600

    view and create entries

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,28 @@
+class EntriesController < ApplicationController
+  def index
+  	@entries = Entry.all
+  end
+
+  def show
+  	@entry = Entry.find(params[:id])
+  end
+
+  def new
+  	@entry = Entry.new
+  end
+	
+	def create
+		if Entry.create(entry_params)
+			redirect_to entries_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def entry_params
+		binding.pry
+		params.require(:entry).permit(:title, :body)
+	end
+end
